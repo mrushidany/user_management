8ee0f34578bdf6b30dfa0363c218bcf99f1213af
Dany Mrushi <mrushidany@gmail.com>
2023-06-09 12:16:31 +0300

    Serial number column

--- resources/js/Components/UserTable.jsx
+++ resources/js/Components/UserTable.jsx
@@ -9,16 +9,16 @@ export default function UserTable({ users }) {
                         <table className="min-w-full divide-y divide-gray-200">
                             <thead className="bg-gray-50">
                                 <tr>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">S/N</th>
                                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
                                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
                                     {/* Add more columns here as needed */}
                                 </tr>
                             </thead>
                             <tbody className="bg-white divide-y divide-gray-200">
-                                {users.map(user => (
+                                {users.map((user, index) => (
                                     <tr key={user.id}>
-                                        <td className="px-6 py-4 whitespace-nowrap">{user.id}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
                                         <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
                                         <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
                                         {/* Add more cells here for each column */}
