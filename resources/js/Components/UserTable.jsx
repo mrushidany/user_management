8cf17da38303ead53778d79744c8c8568e3703cc
Dany Mrushi <mrushidany@gmail.com>
2023-06-09 12:02:35 +0300

    Created the users table component

---
+++ resources/js/Components/UserTable.jsx
@@ -0,0 +1,34 @@
+
+
+export default function UserTable({ users }) {
+  return (
+    <div className="py-12">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="p-6 text-gray-900">
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                                    {/* Add more columns here as needed */}
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                                {users.map(user => (
+                                    <tr key={user.id}>
+                                        <td className="px-6 py-4 whitespace-nowrap">{user.id}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
+                                        {/* Add more cells here for each column */}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
