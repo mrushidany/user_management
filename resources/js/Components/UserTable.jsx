2373529cf02dc48d6bcff539875e1bce679284be
Dany Mrushi <mrushidany@gmail.com>
2023-06-09 12:45:10 +0300

    Successfully deleting and redraw table

--- resources/js/Components/UserTable.jsx
+++ resources/js/Components/UserTable.jsx
@@ -1,64 +1,72 @@
 import axios from "axios";
+import { useState } from "react";
 
 export default function UserTable({ users }) {
-    const handleEdit = (userId) => {
-        // Handle edit action
-    };
+  const [userList, setUserList] = useState(users);
 
-    const handleDelete = (userId) => {
-        // Send a request to delete the user with the specified ID
-        if (confirm('Are you sure you want to delete this user?')) {
-            axios.delete(`/users/${userId}`)
-            .then(response => {
-                console.log(response);
-            })
-            .catch(error => {
-                // Handle error, such as displaying an error message
-                console.error('Error deleting user:', error);
-            });
-        }
-    };
+  const handleEdit = (userId) => {
+    // Handle edit action
+  };
+
+  const handleDelete = (userId) => {
+    // Send a request to delete the user with the specified ID
+    if (confirm('Are you sure you want to delete this user?')) {
+      axios
+        .delete(`/users/${userId}`)
+        .then(response => {
+          console.log(response);
+          // Update the user list after successful deletion
+          const updatedUserList = userList.filter(user => user.id !== userId);
+          setUserList(updatedUserList);
+        })
+        .catch(error => {
+          // Handle error, such as displaying an error message
+          console.error('Error deleting user:', error);
+        });
+    }
+  };
+
   return (
     <div className="py-12">
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div className="p-6 text-gray-900">
-                        <table className="min-w-full divide-y divide-gray-200">
-                            <thead className="bg-gray-50">
-                                <tr>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">S/N</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-200">
-                                {users.map((user, index) => (
-                                <tr key={user.id}>
-                                    <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
-                                    <td className="px-6 py-4 whitespace-nowrap">
-                                    <button
-                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
-                                        onClick={() => handleEdit(user.id)}
-                                    >
-                                        Edit
-                                    </button>
-                                    <button
-                                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                                        onClick={() => handleDelete(user.id)}
-                                    >
-                                        Delete
-                                    </button>
-                                    </td>
-                                </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div className="p-6 text-gray-900">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">S/N</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {userList.map((user, index) => (
+                  <tr key={user.id}>
+                    <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
+                    <td className="px-6 py-4 whitespace-nowrap">{user.name}</td>
+                    <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+                        onClick={() => handleEdit(user.id)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => handleDelete(user.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
